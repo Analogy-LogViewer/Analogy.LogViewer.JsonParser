d28c054b4bffad222b1abab4e6b8d9c629bc8d15
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-02-29 20:48:22 +0200

    update proj structure

---
+++ Analogy.LogViewer.JsonParser.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("620fcc2b-6362-40e5-b545-5d338a66c9ea")]
