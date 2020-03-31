2afece774d8a207cdbdf0134ab0b9dfdbdd7226c
Lior Banai <liorbanai@gmail.com>
2020-03-31 10:08:25 +0300

    update interface

--- Analogy.LogViewer.JsonParser/IAnalogy/AnalogyJsonFactory.cs
+++ Analogy.LogViewer.JsonParser/IAnalogy/AnalogyJsonFactory.cs
@@ -10,61 +10,41 @@ using System.Windows.Forms;
 
 namespace Analogy.LogViewer.JsonParser.IAnalogy
 {
-    public class AnalogyJsonFactory : IAnalogyFactory
+    public class JsonFactory : IAnalogyFactory
     {
-        internal static Guid RssFactoryId = new Guid("D7146342-AEB2-4BD5-8710-7D1BF06EA5CF");
-        public Guid FactoryID { get; } = RssFactoryId;
+        internal static Guid Id = new Guid("D7146342-AEB2-4BD5-8710-7D1BF06EA5CF");
+        public Guid FactoryId { get; } = Id;
         public string Title { get; } = "Analogy Json Text Parser";
-        public IAnalogyDataProvidersFactory DataProviders { get; }
-        public IAnalogyCustomActionsFactory Actions { get; }
         public IEnumerable<IAnalogyChangeLog> ChangeLog => JsonParser.ChangeLog.GetChangeLog();
         public IEnumerable<string> Contributors { get; } = new List<string> { "Lior Banai" };
         public string About { get; } = "Analogy Json Text Parser";
-
-        public AnalogyJsonFactory()
-        {
-            DataProviders = new AnalogyJsonDataProviderFactory();
-            Actions = new AnalogyJsonCustomActionFactory();
-
-        }
-
 
     }
 
     public class AnalogyJsonDataProviderFactory : IAnalogyDataProvidersFactory
     {
+        public Guid FactoryId { get; } = JsonFactory.Id;
         public string Title { get; } = "Analogy XML Data Provider";
-        public IEnumerable<IAnalogyDataProvider> Items { get; }
-
-        public AnalogyJsonDataProviderFactory()
+        public IEnumerable<IAnalogyDataProvider> DataProviders { get; } = new List<IAnalogyDataProvider>()
         {
-            var builtInItems = new List<IAnalogyDataProvider>()
-            {
-                new JsonDataProvider(UserSettingsManager.UserSettings.LogParserSettings)
-            };
-            Items = builtInItems;
-        }
+            new JsonDataProvider(UserSettingsManager.UserSettings.LogParserSettings)
+        };
     }
 
     public class AnalogyJsonCustomActionFactory : IAnalogyCustomActionsFactory
     {
+        public Guid FactoryId { get; } = JsonFactory.Id;
         public string Title { get; } = "Analogy XML Text tools";
-        public IEnumerable<IAnalogyCustomAction> Items { get; }
+        public IEnumerable<IAnalogyCustomAction> Actions { get; } = new List<IAnalogyCustomAction>(0);
 
-        public AnalogyJsonCustomActionFactory()
-        {
-            Items = new List<IAnalogyCustomAction>(0);
-        }
     }
 
     public class AnalogyJsonSettings : IAnalogyDataProviderSettings
     {
 
-        public Guid ID { get; } = new Guid("05E9E0DD-3F3E-4DD8-BFE3-94070212F0F7");
-
         public string Title { get; } = "Plain Text Settings";
         public UserControl DataProviderSettings { get; } = new JsonParserSettings();
-        public Guid FactoryId { get; set; } = AnalogyJsonFactory.RssFactoryId;
+        public Guid FactoryId { get; set; } = JsonFactory.Id;
         public Image SmallImage { get; }
         public Image LargeImage { get; } = Resources.jsonfile32x32;
 
